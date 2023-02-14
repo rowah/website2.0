d0796a3411a89284ada9d23b0c9fe7b277d32b20
Rowa <jrowah@gmail.com>
2023-02-14 21:15:47 +0300

    adds Tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :website, WebsiteWeb.Endpoint,
   secret_key_base: "B4lzkMaOlm4xhkzNM2HiOSpb/f3WsFu3trTS7yHXM2LYzFpcwQ3XswLqJlKG63Cy",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
